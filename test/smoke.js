13d17b6a6fad1073c42ca0e4c13dd0ac6ebe3a8b
U-epsilon10\blecher <stephan@blecher.at>
2015-06-10 22:45:08 +0200

    add processing steps

--- test/smoke.js
+++ test/smoke.js
@@ -1,10 +1,18 @@
 var gitlogger = require('gitlogger.js')
 var gitparser= require('gitparser.js')
+var gitprocessor= require('gitprocessor.js')
+
+var gitparserplugins = [ require('gitparser-plugin-refnames.js'), require('gitparser-plugin-parents.js') ]
+var gitprocessorplugins = [ 
+	require('gitprocessor-plugin-relations.js'), 
+	require('gitparser-plugin-heads.js'), 
+	require('gitparser-plugin-index.js') ]
 
 var loggerCallback = function(text) {
-	gitparser.parse(text)
-};
+	var commits = gitparser(text, gitparserplugins)
+	gitprocessor(commits, gitprocessorplugins);
 
+        console.log("commit: %s", JSON.stringify(commits, null, 2));
+}
 
-
-gitlogger.retrieve(100, loggerCallback);
+gitlogger.retrieve(30, loggerCallback)
